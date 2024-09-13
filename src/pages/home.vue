34d1bc37ff0ca09a9f2f0879c7787e4658f4589b
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-09-13 10:24:09 -0400

    feat: Add modal

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useDessertStore } from '../store/dessertStore';
 import CardComponent from '../components/CardComponent.vue';
 import CartCardComponent from '../components/CartCardComponent.vue';
+import ModalComponent from '../components/ModalComponent.vue';
 import waffleImage from '../assets/images/image-waffle-desktop.jpg';
 import cremeBrulleImage from '../assets/images/image-creme-brulee-desktop.jpg';
 import macaronImage from '../assets/images/image-macaron-desktop.jpg';
@@ -15,8 +16,15 @@ import pannaImage from '../assets/images/image-panna-cotta-desktop.jpg';
 
 const dessertStore = useDessertStore();
 
+const modal = ref(false);
+
 const addToCart = () => {
-    alert('Doce adicionado ao cart');
+    modal.value = true;
+    console.log(modal.value);
+};
+
+const closeModal = () => {
+    modal.value = false;
 };
 
 onMounted(() => {
@@ -77,6 +85,33 @@ onMounted(() => {
                         <CardComponent :card="dessertStore.panna" :src="pannaImage" @click-button="addToCart()"/>
                     </div>
                 </div>
+                <ModalComponent v-if="modal">
+                    <div class="modal fade show d-block bg-light" tabindex="-1" style="--bs-bg-opacity: .5;">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content bg-dark">
+                                <div class="modal-header">
+                                    <h5 class="modal-title text-warning">
+                                        Adicionando itens ao carrinho
+                                    </h5>
+                                    <button type="button" class="btn btn-primary btn-close" @click="closeModal()"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="text-danger">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, voluptatibus doloribus animi iure recusandae temporibus praesentium dolor perferendis voluptates deserunt, dolorem architecto molestias unde. Provident voluptatum deserunt ipsum id unde!
+                                    </p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" @click="closeModal()">
+                                        Fechar
+                                    </button>
+                                    <button type="button" class="btn btn-primary">
+                                        Add ao carrinho
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </ModalComponent>
             </section>
         </div>
     </main>
